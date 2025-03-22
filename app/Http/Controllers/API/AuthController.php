04dfca2177b0a16134234c43832418c0a552ac88
OUSSAMAEDDERKAOUI <edderkaouioussama@gmail.com>
2025-03-22 23:52:42 +0000

    L'AJOUT DE REGISTER METHOD .

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -18,17 +18,48 @@ class AuthController extends Controller
     }
 
     public function register(Request $request){
-        $request->validate([
-            'name' => 'required|string|max:255',
+        
+        // dd($request);
+
+        $validatedData=$request->validate([
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'phone' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6',
+            'password' => 'required|string|min:6',           
+            'status' => 'required|string|in:actif,inactif',
+            'role' => 'required|string|in:admin,recruteur,candidat',
         ]);
 
+        $existingUser = User::where('email', $validatedData['email'])->first();
+
+        if ($existingUser) {
+            return response()->json([
+                'error' => 'The email address is already registered. Please choose a different email.',
+            ], 422); 
+        }
+        
+        if ($request->hasFile('photo')) {
+            $photoPath = $request->file('photo')->store('profil_photos', 'public');
+        } else {
+            $photoPath = null;  
+        }
+    
+  
+// dd($photoPath);
+
         $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
+            'first_name' => $validatedData['first_name'],   
+            'last_name' => $validatedData['last_name'],     
+            'phone' => $validatedData['phone'],             
+            'photo' => $photoPath,             
+            'email' => $validatedData['email'],             
+            'password' => Hash::make($validatedData['password']),  
+            'status' => $validatedData['status'],           
+            'role' => $validatedData['role'],               
         ]);
+        
+        
 
         $token = Auth::guard('api')->login($user);
         return response()->json([
