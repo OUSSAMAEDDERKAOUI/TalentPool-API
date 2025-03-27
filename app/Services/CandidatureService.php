e842170a4a6f0105854dca7e6da595c671e4aa9b
OUSSAMAEDDERKAOUI <edderkaouioussama@gmail.com>
2025-03-27 01:43:02 +0000

    la creation de candidature service avec la methode registercandidature .

---
+++ app/Services/CandidatureService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Candidature;
+use App\Repositories\CandidatureRepository;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class CandidatureService
+{
+    protected $CandidatureRepository;
+
+    public function __construct(CandidatureRepository $CandidatRepository)
+    {
+        $this->CandidatureRepository = $CandidatRepository;
+    }
+
+    public function registercandidature(array $candidatureData)
+    {
+            
+            
+            $candidature = $this->CandidatureRepository->createcandidature($candidatureData);
+
+
+            return $candidature;
+
+    }
+  
+
+     
+  
+}
+
