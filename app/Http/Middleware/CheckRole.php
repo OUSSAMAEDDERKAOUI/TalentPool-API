ff5af5b4413323d9bb6c849a6eb9b17581ea8d8d
OUSSAMAEDDERKAOUI <edderkaouioussama@gmail.com>
2025-03-28 12:12:18 +0000

    L'ajout demiddleware checkRole  .

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -13,15 +13,34 @@ class CheckRole
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next,...$roles)
-    {
+    public function handle(Request $request, Closure $next, ...$roles)
+{
+    $user = $request->user();
 
-        $user = $request->user();
-
-        if (! $user || ! in_array($user->role, $roles)) {
-            abort(403, 'Unauthorized');
-        }
-
-        return $next($request);
+    if (!$user) {
+        return response()->json([
+            'error' => 'User is not authenticated',
+            'message' => 'You need to be logged in to access this resource.'
+        ], Response::HTTP_UNAUTHORIZED);
     }
+
+    if (!$this->hasValidRole($user, $roles)) {
+        return response()->json([
+            'error' => 'Forbidden',
+            'message' => 'You do not have the required permissions to access this resource.'
+        ], Response::HTTP_FORBIDDEN);
+    }
+
+    return $next($request);
 }
+
+
+private function hasValidRole($user, $roles)
+{
+    // Vérifier le rôle de l'utilisateur 
+    return in_array($user->role, $roles);
+}
+
+}
+
+
--- routes/api.php
+++ routes/api.php
@@ -13,18 +13,27 @@ use App\Http\Controllers\API\AuthController;
 
 
 
-// Route::prefix('auth')->group(function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
-    Route::post('refresh', [AuthController::class, 'refresh']);
-    Route::post('logout', [AuthController::class, 'logout']);
-// });
 
-Route::middleware('auth:api')->group(function () {
+    Route::middleware('auth:api')->group(function(){
+        Route::post('refresh', [AuthController::class, 'refresh'])->middleware('auth:api');
+        Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:api');
+    });
+       
+
+Route::group(['middleware'=>['auth:api','CheckRole:recruteur']],function(){
+
     Route::apiResource('annonce', AnnonceController::class);  
+    Route::get('annonce/{annonceId}/candidatures', [AnnonceController::class, 'getCandidaturesByAnnonce']);
+
+});
+
+
+Route::group(['middleware'=>['auth:api','CheckRole:candidat']],function(){
 
     Route::apiResource('candidature', CandidatureController::class);  
     Route::post('candidature/{candidature}', [CandidatureController::class, 'update']);  
 
-    Route::get('annonce/{annonceId}/candidatures', [AnnonceController::class, 'getCandidaturesByAnnonce']);
 });
+
